f860fe8e1903234044bcd140db87f8498839dafe
Михаил <tarassenko.m.v@live.com>
2018-09-11 21:55:47 +0300

    "this" make dream happens

--- NeuralNetwork/Matrix.cpp
+++ NeuralNetwork/Matrix.cpp
@@ -17,20 +17,20 @@ Matrix::Matrix(int nrow, int ncol) {
 	}
 };
 
-Matrix Matrix::operator+(Matrix res) {
-	if ((res.size[0] != size[0]) || (res.size[1] != size[1]))
+Matrix* Matrix::operator+(Matrix* res) {
+	if ((res->size[0] != size[0]) || (res->size[1] != size[1]))
 		throw 1;
 	for (int i = 0; i < size[0]; i++)
 	{
 		for (int k = 0; k < size[1]; k++)
 		{
-			res.matrix[i][k] += this->matrix[i][k];
+			res->matrix[i][k] += this->matrix[i][k];
 		}
 	}
 	return res;
 };
 
-Matrix Matrix::operator+(float num) {
+Matrix* Matrix::operator+(float num) {
 	for (int i = 0; i < size[0]; i++)
 	{
 		for (int k = 0; k < size[1]; k++)
@@ -38,23 +38,23 @@ Matrix Matrix::operator+(float num) {
 			this->matrix[i][k] += num;
 		}
 	}
-	return *this;
+	return this;
 };
 
-Matrix Matrix::operator-(Matrix res) {
-	if ((res.size[0] != size[0]) || (res.size[1] != size[1]))
+Matrix* Matrix::operator-(Matrix* res) {
+	if ((res->size[0] != size[0]) || (res->size[1] != size[1]))
 		throw 1;
 	for (int i = 0; i < size[0]; i++)
 	{
 		for (int k = 0; k < size[1]; k++)
 		{
-			res.matrix[i][k] -= this->matrix[i][k];
+			res->matrix[i][k] -= this->matrix[i][k];
 		}
 	}
 	return res;
 };
 
-Matrix Matrix::operator-(float num) {
+Matrix* Matrix::operator-(float num) {
 	for (int i = 0; i < size[0]; i++)
 	{
 		for (int k = 0; k < size[1]; k++)
@@ -62,23 +62,23 @@ Matrix Matrix::operator-(float num) {
 			this->matrix[i][k] -= num;
 		}
 	}
-	return *this;
+	return this;
 };
 
-Matrix Matrix::operator*(Matrix res) {
-	if ((res.size[0] != size[0]) || (res.size[1] != size[1]))
+Matrix* Matrix::operator*(Matrix* res) {
+	if ((res->size[0] != size[0]) || (res->size[1] != size[1]))
 		throw 1;
 	for (int i = 0; i < size[0]; i++)
 	{
 		for (int k = 0; k < size[1]; k++)
 		{
-			res.matrix[i][k] *= this->matrix[i][k];
+			res->matrix[i][k] *= this->matrix[i][k];
 		}
 	}
 	return res;
 };
 
-Matrix Matrix::operator*(float num) {
+Matrix* Matrix::operator*(float num) {
 	for (int i = 0; i < size[0]; i++)
 	{
 		for (int k = 0; k < size[1]; k++)
@@ -86,18 +86,18 @@ Matrix Matrix::operator*(float num) {
 			this->matrix[i][k] *= num;
 		}
 	}
-	return *this;
+	return this;
 };
 
-Matrix Matrix::dot(Matrix m) {
-	if (this->size[1] != m.size[0])
+Matrix* Matrix::dot(Matrix* m) {
+	if (this->size[1] != m->size[0])
 		throw 2;
-	Matrix res(this->size[0], m.size[1]);
+	Matrix* res = new Matrix(this->size[0], m->size[1]);
 	for (int i = 0; i < size[0]; i++) {
-		for (int j = 0; j < m.size[1]; j++) {
+		for (int j = 0; j < m->size[1]; j++) {
 			for (int k = 0; k < this->size[1]; k++)
 			{
-				res.matrix[i][j] += this->matrix[i][k] * m.matrix[k][j];
+				res->matrix[i][j] += this->matrix[i][k] * m->matrix[k][j];
 			}
 		}
 	}
@@ -117,13 +117,13 @@ float Matrix::norm() {
 	return res;
 }
 
-Matrix Matrix::copy() {
-	Matrix res(size[0], size[1]);
+Matrix* Matrix::copy() {
+	Matrix* res = new Matrix(size[0], size[1]);
 	for (int i = 0; i < size[0]; i++)
 	{
 		for (int k = 0; k < size[1]; k++)
 		{
-			res.matrix[i][k] = matrix[i][k];
+			res->matrix[i][k] = matrix[i][k];
 		}
 	}
 	return res;
@@ -141,6 +141,9 @@ void Matrix::print() {
 }
 
 Matrix::~Matrix() {
-	
-	delete matrix;
+	for (size_t i = 0; i < size[0]; i++)
+	{
+		free(matrix[i]);
+	}
+	free(matrix);
 };
